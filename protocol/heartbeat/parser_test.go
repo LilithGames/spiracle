174ffbadcccaeb0f0c71b3068056075d4836759d
hulucc <hulucc@outlook.com>
2023-01-16 10:00:48 +0800

    fix: heartbeat check parser

--- protocol/heartbeat/parser_test.go
+++ protocol/heartbeat/parser_test.go
@@ -7,11 +7,11 @@ import (
 
 func TestParser(t *testing.T) {
 	p := Parser()
-	token, err := p([]byte("x1234 xxxxx"))
+	token, err := p([]byte("1234 xxxxx"))
 	assert.Nil(t, err)
 	assert.Equal(t, uint32(1234), token)
-	token, err = p([]byte("x1234xxxxx"))
+	token, err = p([]byte("1234xxxxx"))
 	assert.NotNil(t, err)
-	token, err = p([]byte("xhello xxxxx"))
+	token, err = p([]byte("hello xxxxx"))
 	assert.NotNil(t, err)
 }
